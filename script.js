b3d211a4233d90c416650d164f7cb74fa9748a75
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-08-25 01:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let nameElement = document.querySelector("#fname");
 
-nameElement.addEventListener("input", function () {
+nameElement.addEventListener("blur", function () {
   nameElement.value = nameElement.value.toUpperCase();
 });
